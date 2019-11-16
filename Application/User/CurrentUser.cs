1cdc52b8cb937e483f73e32cf204b4bd1b57fbdd
Getoar <g.sadiku@vision-agency.com>
2019-11-16 01:28:59 +0100

    Identity added

---
+++ Application/User/CurrentUser.cs
@@ -0,0 +1,54 @@
+﻿using Application.Errors;
+using Application.Interfaces;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.User
+{
+    public class CurrentUser
+    {
+        public class Query : IRequest<User>
+        {
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, User>
+        {
+            private readonly UserManager<AppUser> _userManager;
+            private readonly IUserAccessor _userAccessor;
+            private readonly IJwtGenerator _jwtGenerator;
+            public Handler(UserManager<AppUser> userManager, IJwtGenerator jwtGenerator, IUserAccessor userAccessor)
+            {
+                _jwtGenerator = jwtGenerator;
+                _userAccessor = userAccessor;
+                _userManager = userManager;
+            }
+
+            public async Task<User> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var user = await _userManager.FindByNameAsync(_userAccessor.GetCurrentUserName());
+
+                //if (user == null)
+                //    throw new RestException(HttpStatusCode.Unauthorized);
+
+                return new User
+                {
+                    DisplayName = user.DisplayName,
+                    Token = _jwtGenerator.CreateToken(user),
+                    UserName = user.UserName,
+                    Image = null
+                };
+
+                throw new RestException(HttpStatusCode.Unauthorized);
+            }
+        }
+    }
+}
